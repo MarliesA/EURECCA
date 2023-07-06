396ebc80cf81aee95ac03785cdd3aed9645619b3
marliesa <m.a.vanderlugt@tudelft.nl>
2023-07-06 14:54:04 +0200

    config read fix 5

--- processing_scripts/SEDMEX_processing/reduce_tailored_timeseries.py
+++ processing_scripts/SEDMEX_processing/reduce_tailored_timeseries.py
@@ -11,7 +11,7 @@ config = yaml.safe_load(Path('sedmex-processing.yml').read_text())
 def combine_ncs(instrument, config):
     instrumentFolder = os.path.join(config['experimentFolder'], instrument)
 
-    fold = os.path.join(instrumentFolder, 'tailored', r'*.nc')
+    fold = os.path.join(instrumentFolder, 'tailored', r'L*_*.nc')
     filez = glob.glob(fold)
 
     dslist = []
@@ -34,20 +34,45 @@ if __name__ == "__main__":
     if not config['instruments']['adcp'] == None:
         allInstruments += config['instruments']['adcp']
         instrumentType += ['adcp']
+    if not config['instruments']['ossi'] == None:
+        allInstruments += config['instruments']['ossi']
+        instrumentType += ['pres']
+    if not config['instruments']['solo'] == None:
+        allInstruments += config['instruments']['solo']
+        instrumentType += ['pres']
 
     for instrument, Type in zip(allInstruments, instrumentType):
+        print(instrument)
 
-        # only keep coord t and merge all days
-        ds = combine_ncs(instrument, config)
+        if Type == 'pres':
+            ds = xr.open_dataset(os.path.join(config['experimentFolder'], instrument, 'tailored', 'instrument.nc'))
+            ds = ds.drop_dims(['f', 'N'], errors='ignore')
+            # update the summary
+            ds.attrs['summary'] = 'tailored timeseries of wave statistics'
+        else:
+            # only keep coord t and merge all days
+            ds = combine_ncs(instrument, config)
+            # update the summary
+            ds.attrs['summary'] = 'tailored timeseries of wave and current statistics'
+
+        # on the repository we only publish the most relevant parameters, so drop the others
+        vars2drop = ['Skc', 'Asc', 'sigc', 'Skl', 'Asl', 'sigl', 'Skp0', 'Asp0', 'sigp0', 'Skp', 'Asp', 'sigp',
+                     'Sk0', 'As0', 'sig0', 'svdtheta', 'svddspr', 'fp', 'udm', 'vdm', 'ud_ssm']
+
+        ds = ds.drop_vars(vars2drop, errors='ignore')
 
         # resample to timeseries at burstduration interval, such that missing data get awarded a nan.
         t_resolution = '{}s'.format(config['burstDuration'][Type])
         ds = ds.resample(t=t_resolution).nearest(tolerance=t_resolution)
 
+
         # update the construction date
-        ds.attrs['construction datetime']: datetime.now().strftime("%d-%b-%Y (%H:%M:%S)")
+        ds.attrs['construction datetime'] = datetime.now().strftime("%d-%b-%Y (%H:%M:%S)")
+
+
 
         # update script version information
         ds.attrs['git hash'] = get_githash()
 
-        ds.to_netcdf(os.path.join(config['experimentFolder'], instrument, 'tailored', 'tailored_' + instrument + '.nc'), encoding=ds.encoding)
+        ds.to_netcdf(os.path.join(config['experimentFolder'], instrument, 'tailored', 'tailored_' + instrument + '.nc'),
+                     encoding=ds.encoding)
